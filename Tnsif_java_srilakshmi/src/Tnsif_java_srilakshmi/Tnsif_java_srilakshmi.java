b4f3ab7866abeac04298a0ce7d619cbba470c527
Administrator <Administrator@WINDOWS-41RDJB5>
2025-09-30 08:56:53 +0530

    commit 1

---
+++ Tnsif_java_srilakshmi/src/Tnsif_java_srilakshmi/Tnsif_java_srilakshmi.java
@@ -0,0 +1,13 @@
+package Tnsif_java_srilakshmi;
+
+public class Tnsif_java_srilakshmi {
+  public static void main(String [] args) {
+		int a =Integer.parseInt(args[0]);
+		int b =Integer.parseInt(args[1]);
+		int c=a+b;
+		System.out.println(c);
+		
+	}
+	 
+
+}
---
+++ Tnsif_java_srilakshmi/src/basic/AdditionScanner.java
@@ -0,0 +1,14 @@
+package basic;
+import java.util.*;
+
+public class AdditionScanner {
+	public static void main(String[]args) {
+		Scanner s= new Scanner(System.in);
+		
+		int a =s.nextInt();
+		int b =s.nextInt();
+		int c=a+b;
+		System.out.println(c);
+	} 
+
+}
